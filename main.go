779dd9a8ecbe7d1a99e104281920eaae519f4289
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2018-02-18 19:27:33 +0100

    WIP: add consul registration, prometheus config and test stack file

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"context"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/client"
+	consulapi "github.com/hashicorp/consul/api"
 	log "github.com/sirupsen/logrus"
+)
+
+var (
+	e error
 )
 
 func init() {
@@ -32,28 +39,72 @@ func init() {
 }
 
 func main() {
+	client, _ := consulapi.NewClient(consulapi.DefaultConfig())
+	catalog := client.Catalog()
 	tc := tasks()
 	for t := range tc {
 		if t.Status.State == "running" {
 			s := service(t.ServiceID)
-			// get ports from servic e labels, means we need to change orchestra to populate those
-			log.Info("running task ", t.ID, " belongs to service ", s.Spec.Name, " labels ", s.Spec.Labels)
-			// client, _ := consulapi.NewClient(consulapi.DefaultConfig())
-			// catalog := client.Catalog()
-			// consulRegistration := createConsulRegistration(s swarm.Service, t swarm.Task)
-			// _, e := catalog.Register(consulRegistration, nil)
-			// if e == nil {
-			// 	log.Info("Service registered in Consul")
-			// } else {
-			// 	log.Error("Connection to consul failed: ", e)
-			// }
+			for _, v := range t.NetworksAttachments {
+				if v.Network.Spec.Name != "ingress" {
+					log.Debug("v is: ", v.Addresses, v.Network.Spec.Name)
+					log.Info("running task ", t.ID, " belongs to service ", s.Spec.Name, " with labels ", s.Spec.Labels)
+					m := convertPort(s.Spec.Labels)
+					ip := strings.Split(v.Addresses[0], "/")[0]
+					//
+					for l, p := range m {
+						ccr := createCatalogRegistration(s.Spec.Name, l, t.ID, ip, p)
+						registerInConsul(ccr, catalog)
+					}
+				}
+			}
 		}
 	}
 }
 
-// func createConsulRegistration() *consulapi.CatalogRegistration {
-//bla
-// }
+func registerInConsul(cr consulapi.CatalogRegistration, c *consulapi.Catalog) {
+	_, e := c.Register(&cr, nil)
+	if e != nil {
+		log.Error("failed to register ", cr.Service.Service, " in consul: ", e)
+	}
+}
+
+func createCatalogRegistration(name, label, taskID, ip string, p int) consulapi.CatalogRegistration {
+	var (
+		cas consulapi.AgentService
+		ccr consulapi.CatalogRegistration
+	)
+	sp := strconv.Itoa(p)
+	// cas.ID = s.Spec.Name + ":" + string(p)
+	cas.Service = name + ":" + sp
+	cas.Tags = []string{taskID, label, sp, "prometheus"}
+	cas.Port = p
+	cas.Address = ip
+
+	// ccr.ID = s.Spec.Name + ":" + string(p)
+	ccr.Node = "prometheus"
+	ccr.Address = "127.0.0.1"
+	ccr.TaggedAddresses = map[string]string{"wan": "121212", "lan": "1212121"}
+	ccr.Datacenter = "dc1"
+	ccr.Service = &cas
+	log.Debug(ccr)
+	return ccr
+}
+
+func convertPort(l map[string]string) map[string]int {
+	m := make(map[string]int)
+	for i, v := range l {
+		log.Debug("debug: ", i, v)
+		if strings.HasPrefix(i, "prometheus.metrics") {
+			p, e := strconv.Atoi(v)
+			if e != nil {
+				log.Error("failed to convert label value for: ", i, v)
+			}
+			m[i] = p
+		}
+	}
+	return m
+}
 
 func tasks() <-chan swarm.Task {
 	c := make(chan swarm.Task)
